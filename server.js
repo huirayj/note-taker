285d75d8eae87c461c3213d35caaaf2e4c2417ed
Justin Hui <huirayj@gmail.com>
2021-06-12 15:30:21 -0400

    removed develop path, added server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+app.get('/api/notes', (req, res) => res.json());
+
+app.listen(PORT, () => console.log(`App listening on: http://localhost:${PORT}`));
