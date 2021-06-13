48326d6a5beee8d8f966bf31a9ab56e3afcedf74
Justin Hui <huirayj@gmail.com>
2021-06-13 14:52:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+let notes = require('./db/db.json');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-
-app.get('/api/notes', (req, res) => res.json());
 
 app.listen(PORT, () => console.log(`App listening on: http://localhost:${PORT}`));
