d9fd0a075293144dd9e526427760310802dc48e8
Justin Hui <huirayj@gmail.com>
2021-06-13 15:23:23 -0400

    added delete method

--- server.js
+++ server.js
@@ -11,14 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/api/notes', (req, res) => {
+app.get('/api/notes', (req, res, next) => {
     fs.readFile('db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        res.json(JSON.parse(data));
+        (err) ? next(err) : res.json(JSON.parse(data));
     });
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
     const newNote = req.body;
     let newId = notes.length;
 
@@ -27,11 +26,33 @@ app.post('/api/notes', (req, res) => {
     notes.push(newNote);
 
     fs.writeFile('db/db.json', JSON.stringify(notes), err => {
-        if (err) { console.log(err) };
-        console.log('Successfully saved.')
+        (err) ? next(err) : console.log('Successfully saved.');
     });
 
     return res.status(200).end();
+});
+
+app.delete('/api/notes/:id', (req, res, next) => {
+    const id = req.params.id;
+
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        let updatedId = 0;
+        let updatedNotes = JSON.parse(data).filter(note => note.id !== id);
+
+        updatedNotes.map(note => {
+            note.id = updatedId.toString();
+            updatedId++;
+        });
+
+        fs.writeFile('db/db.json', JSON.stringify(updatedNotes), (err) => {
+            if (err) {
+                next(err);
+            } else {
+                console.log('Successfully deleted');
+                res.json(updatedNotes);
+            }
+        });
+    });
 });
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
